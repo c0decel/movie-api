71ede2ea558aecc73f77381988a5a5ee1253f696
c0decel <124975714+c0decel@users.noreply.github.com>
2023-07-17 21:18:30 -0700

    removed auth from /movies

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/', (req, res) => {
 
 
 // get movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
 	Movies.find()
 		.then((movies) => {
 			res.status(201).json(movies);
