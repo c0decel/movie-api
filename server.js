aaf0e23d9821d37b777e98ae6c85cf62748e3f25
c0decel <124975714+c0decel@users.noreply.github.com>
2023-08-15 17:48:12 -0700

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'https://movie-apis-84b92f93a404.herokuapp.com', 'https://codesandbox.io/s/thirsty-http-8jlvsv', 'http://localhost:1234', 'https://main--movies-api-client.netlify.app/'];
+let allowedOrigins = ['http://localhost:8080', 'https://movie-apis-84b92f93a404.herokuapp.com', 'https://codesandbox.io/s/thirsty-http-8jlvsv', 'http://localhost:1234', 'https://main--movies-api-client.netlify.app'];
 
 app.use(cors({
   origin: (origin, callback) => {
